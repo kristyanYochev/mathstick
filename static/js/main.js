3478c916f51ef9907ff8543abbab6a03b5db1a01
kristyanYochev <kris.yochev@gmail.com>
2019-03-15 13:15:38 +0200

    [Update] Added single symbol rendering and reading

--- static/js/main.js
+++ static/js/main.js
@@ -12,9 +12,79 @@ stage.interactive = true
 var matches_manager
 var displays_manager
 
+/**
+ * Segments are numebered like so
+ * 
+ * Number symbol
+ * 
+ *    +---0---+
+ *    |       |
+ *    5       1
+ *    |       |
+ *    +---6---+
+ *    |       |
+ *    4       2
+ *    |       |
+ *    +---3---+
+ * 
+ * Operator symbol
+ * 
+ *       |
+ *       1
+ *    -0-+---
+ *       |
+ *       |
+ * 
+ * Equals symbol
+ * 
+ *   ---0---
+ *   ---1---
+ */
 ////////////////////////////////////////////////////////////
 const MATCH_LENGTH = 70
 const SNAP_DISTANCE = 30
+const PLAY_AREA_X = 200
+const PLAY_AREA_Y = 200
+const DISPLAY_SEGMENTS = {
+    number: [
+        {x: MATCH_LENGTH / 2, y: 0, angle: 0, interactive: true},
+        {x: MATCH_LENGTH, y: MATCH_LENGTH / 2, angle: 3 * Math.PI / 2, interactive: true},
+        {x: MATCH_LENGTH, y: 3 * MATCH_LENGTH / 2, angle: 3 * Math.PI / 2, interactive: true},
+        {x: MATCH_LENGTH / 2, y: 2 * MATCH_LENGTH, angle: 0, interactive: true},
+        {x: 0, y: 3 * MATCH_LENGTH / 2, angle: 3 * Math.PI / 2, interactive: true},
+        {x: 0, y: MATCH_LENGTH / 2, angle: 3 * Math.PI / 2, interactive: true},
+        {x: MATCH_LENGTH / 2, y: MATCH_LENGTH, angle: 0, interactive: true}
+    ],
+    operator: [
+        {x: MATCH_LENGTH / 2, y: MATCH_LENGTH, angle: 0, interactive: true},
+        {x: MATCH_LENGTH / 2, y: MATCH_LENGTH, angle: 3 * Math.PI / 2, interactive: true}
+    ],
+    equal_sign: [
+        {x: MATCH_LENGTH / 2, y: 3 * MATCH_LENGTH / 4, angle: 0, interactive: false},
+        {x: MATCH_LENGTH / 2, y: 5 * MATCH_LENGTH / 4, angle: 0, interactive: false}
+    ]
+}
+const MAP_SYMBOLS_TO_SEGMENTS = {
+    number: {
+        '0': [0, 1, 2, 3, 4, 5],
+        '1': [1, 2],
+        '2': [0, 1, 6, 4, 3],
+        '3': [0, 1, 6, 2, 3],
+        '4': [5, 6, 1, 2],
+        '5': [0, 5, 6, 2, 3],
+        '6': [0, 5, 6, 4, 2, 3],
+        '7': [0, 1, 2],
+        '8': [0, 1, 2, 3, 4, 5, 6],
+        '9': [0, 5, 6, 1, 2, 3]
+    },
+    operator: {
+        '-': [0],
+        '+': [0, 1]
+    },
+    equal_sign: {
+        '=': [0, 1]
+    }
+}
 
 ////////////////////////////////////////////////////////////
 PIXI.loader
--- static/js/utils.js
+++ static/js/utils.js
@@ -1,4 +1,16 @@
-function distance_between_points(x1, x2, y1, y2)
+function distance_between_points(x1, y1, x2, y2)
 {
-    return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+    return Math.sqrt(
+        ((x1-x2) ** 2) + ((y1-y2) ** 2)
+    )
+}
+
+function conatins_all(arr1, arr2)
+{
+    return arr1.every(arr1_item => arr2.includes(arr1_item))
+}
+
+function arrays_have_same_members(arr1, arr2)
+{
+    return conatins_all(arr1, arr2) && conatins_all(arr2, arr1)
 }
