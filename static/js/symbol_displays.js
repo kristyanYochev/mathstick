b6f75c90a9347e2df03c7f8b7a00b7676ce0f331
kristyanYochev <kris.yochev@gmail.com>
2019-03-15 12:01:17 +0200

    [Update] Added removal of matches

--- static/js/symbol_displays.js
+++ static/js/symbol_displays.js
@@ -8,6 +8,43 @@ class Segment
         this.angle = angle
         this.interactive = interactive
     }
+
+    has_matchstick()
+    {
+        for (var stick of matches_manager.matches)
+        {
+            if (
+                stick.sprite.position.x == this.x &&
+                stick.sprite.position.y == this.y &&
+                stick.sprite.rotation == this.angle
+            )
+            {
+                return true
+            }
+        }
+
+        return false
+    }
+
+    add_matchstick()
+    {
+        if (this.has_matchstick())
+        {
+            return
+        }
+
+        matches_manager.add_matchstick(this.x, this.y, this.angle, this.interactive)
+    }
+
+    remove_matchstick()
+    {
+        if (!this.has_matchstick())
+        {
+            return
+        }
+
+        matches_manager.remove_matchstick_by_position(this.x, this.y, this.angle)
+    }
 }
 
 class SymbolDisplay
