7e6fee6ff31fdc6005da5ad87684464de195e0aa
Georgi Korchakov <georgikorchakov@gmail.com>
2019-03-15 10:33:17 +0200

    [Update] Added route for index (login register) page

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
+# For Flask
 from flask import Flask, render_template, request, redirect, session, jsonify
+from config import HOST, PORT
 
+# For Database
 import pymysql
+from passlib.hash import sha256_crypt as sha256
+from config import DATABASE_HOST, \
+    DATABASE_USER, DATABASE_PASSWORD, DATABASE_DATABASE
+
+db = pymysql.connect(
+    host=DATABASE_HOST,
+    user=DATABASE_USER,
+    password=DATABASE_PASSWORD,
+    database=DATABASE_DATABASE,
+    cursorclass=pymysql.cursors.DictCursor
+)
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "fjwwuhf72te8tr$^E$RIYT^E#%@$_))&^$W@>><GHtdwygif37ytr68fr3yhuiahfi7ybw7b"
 
+@app.route("/")
+def index():
+    return render_template("LoginRegister.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host=HOST, port=int(PORT))
