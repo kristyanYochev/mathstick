748133e111b26b4f3fe17b2c8e516fe705a6cfcd
Ivan Dimitrov <ivan.dimitrov293@gmail.com>
2019-03-15 13:32:28 +0200

    [Update] better rendering canvas

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,7 @@ const CANVAS_HEIGHT = CANVAS_WIDTH * (9 / 16)
 
 var renderer = PIXI.autoDetectRenderer(CANVAS_WIDTH, CANVAS_HEIGHT)
 
-document.body.appendChild(renderer.view)
+document.getElementById("game").appendChild(renderer.view)
 
 ////////////////////////////////////////////////////////////
 var stage = new PIXI.Container()
