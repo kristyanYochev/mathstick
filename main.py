bedaede9a6d5259397633e451f3d4e03b6130871
Georgi Korchakov <georgikorchakov@gmail.com>
2019-03-15 14:50:34 +0200

    [Update] Added sockets in main.py

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ from passlib.hash import sha256_crypt as sha256
 from config import DATABASE_HOST, \
     DATABASE_USER, DATABASE_PASSWORD, DATABASE_DATABASE
 
+# Sockets
+from flask_socketio import SocketIO, join_room, leave_room, emit
+
+# Utils
+import random
+import uuid
+
 db = pymysql.connect(
     host=DATABASE_HOST,
     user=DATABASE_USER,
@@ -18,7 +25,86 @@ db = pymysql.connect(
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "fjwwuhf72te8tr$^E$RIYT^E#%@$_))&^$W@>><GHtdwygif37ytr68fr3yhuiahfi7ybw7b"
+socketio = SocketIO(app)
 
+'''
+---------- Utils ----------
+'''
+
+def get_unique_id():
+    unique_id = str(uuid.uuid4())
+    return unique_id[-6:]
+
+def get_equations(number_of_equations, user_ids):
+    sql_users = ""
+
+    for user_id in user_ids:
+        sql_users += str(user_id) + ", "
+
+    sql_users = sql_users[:-2]
+
+    sql_query = "SELECT * FROM equations WHERE id NOT IN (SELECT equation_id FROM completed WHERE user_id in ({})) ORDER BY RAND() LIMIT {}".format(
+        pymysql.escape_string(sql_users),
+        pymysql.escape_string(str(number_of_equations))
+    )
+
+    with db.cursor() as cursor:
+        cursor.execute(sql_query)
+        equations = cursor.fetchall()
+
+    return equations
+
+
+'''
+---------- Sockets ----------
+'''
+
+@socketio.on("craete_room")
+def on_create_room(data):
+    room_id = get_unique_id()
+    join_room(room_id)
+    emit("created_room", {"room_id": room_id}, room=room_id)
+
+
+@socketio.on("join_room")
+def on_join_room(data):
+    room_id = data["room_id"]
+    username = data["username"]
+    user_id = data["user_id"]
+
+    join_room(room_id)
+    emit("joined_room", {"username": username, "user_id": user_id}, room=room_id)
+
+
+@socketio.on("start_game")
+def on_start_game(data):
+    room_id = data["room_id"]
+    user_ids = data["user_ids"]
+    equations_count = int(data["equations_count"])
+
+    equations = get_equations(equations_count, user_ids)
+
+    emit("starting_game", {"equations": equations}, room=room_id)
+
+
+@socketio.on("finish_game")
+def on_finish_game(data):
+    room_id = data["room_id"]
+    user_id = data["user_id"]
+    time = data["time"]
+
+    emit("finished_game", {"user_id": user_id, "time": time}, room=room_id)
+
+
+@socketio.on("leave_room")
+def on_leave_room(data):
+    room_id = data["room_id"]
+    leave_room(room_id)
+
+
+'''
+---------- Routes ----------
+'''
 
 @app.route("/")
 def index():
@@ -109,6 +195,7 @@ def get_equation():
 
     return jsonify(equation)
 
+
 @app.route("/complete", methods=["POST"])
 def completed():
     json_data = request.get_json()
@@ -129,4 +216,4 @@ def completed():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host=HOST, port=int(PORT))
+    socketio.run(app, debug=True, host=HOST, port=int(PORT))
