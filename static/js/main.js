0934217165f96579e5185ed2ebe9fdee2ce1a35f
kristyanYochev <kris.yochev@gmail.com>
2019-03-17 01:47:27 +0200

    [Fix] Display usernames instead of ids

--- static/js/main.js
+++ static/js/main.js
@@ -180,7 +180,7 @@ function finish_game()
                 socket.emit('finish_game', {
                     room_id: sessionStorage.getItem('room_id'),
                     user_id: uid,
-                    username: username,
+                    username: document.getElementById('username').value,
                     time: time_taken / 1000
                 })
             }
@@ -309,7 +309,7 @@ function init()
 
         socket.on('finished_game', function(data) {
             var player_element = document.createElement('p')
-            player_element.innerText = data.user_id + ' - ' + data.time + 's'
+            player_element.innerText = data.username + ' - ' + data.time + 's'
 
             document.getElementById('players').appendChild(player_element)
         })
